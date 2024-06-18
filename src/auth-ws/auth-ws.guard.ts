21af4251273895416bc282cd3f0bfcc07e2897bc
vitJR1 <kalugin.vitaliy23@gmail.com>
2024-06-18 08:49:00 +0500

    feat: check ws authorization by token in headers

--- src/auth-ws/auth-ws.guard.ts
+++ src/auth-ws/auth-ws.guard.ts
@@ -13,15 +13,19 @@ export class AuthWsGuard implements CanActivate {
 
   async canActivate(context: ExecutionContext): Promise<boolean> {
     const ws = context.switchToWs();
-    const token = this.extractTokenFromWsPayload(ws);
+    const token =
+      this.extractTokenFromWsPayload(ws) ??
+      this.extractTokenFromWsRequestHeaders(ws);
     if (!token) {
       throw new UnauthorizedException();
     }
+    console.log(token);
     try {
       ws.getClient()['data'] = await this.jwt.verifyAsync(token, {
         ignoreExpiration: true,
       });
-    } catch {
+    } catch (e) {
+      console.log(e);
       throw new UnauthorizedException();
     }
     return true;
@@ -29,6 +33,14 @@ export class AuthWsGuard implements CanActivate {
 
   private extractTokenFromWsPayload(ws: WsArgumentsHost): string | undefined {
     const [type, token] = ws.getClient().handshake.auth.token?.split(' ') ?? [];
+    return type === 'Bearer' ? token : undefined;
+  }
+
+  private extractTokenFromWsRequestHeaders(
+    ws: WsArgumentsHost,
+  ): string | undefined {
+    const [type, token] =
+      ws.getClient().handshake.headers.authorization?.split(' ') ?? [];
     return type === 'Bearer' ? token : undefined;
   }
 }
