ae6601e9ce1972ea6e8034d08055b3393861b124
vitJR1 <kalugin.vitaliy23@gmail.com>
2024-09-12 14:27:39 +0500

    feat: check subnet

--- src/modules/kaspiapi/kaspiapi.guard.ts
+++ src/modules/kaspiapi/kaspiapi.guard.ts
@@ -5,27 +5,36 @@ import {
   ForbiddenException,
 } from '@nestjs/common';
 import { Request } from 'express';
+import * as ipRangeCheck from 'ip-range-check';
 
 @Injectable()
 export class IpAndMethodGuard implements CanActivate {
-  private readonly allowedIp = '194.187.247.152';
+  private readonly allowedSubnet = '194.187.247.0/24';
   private readonly allowedMethod = 'GET';
 
   canActivate(context: ExecutionContext): boolean {
     const request: Request = context.switchToHttp().getRequest();
-    const clientIp = request.ip;
     const method = request.method;
 
-    console.log('IpAndMethodGuard', { clientIp, method, request });
-
     if (method !== this.allowedMethod) {
+      console.log('Only GET requests are allowed');
       throw new ForbiddenException('Only GET requests are allowed');
     }
 
-    if (!clientIp.endsWith(this.allowedIp)) {
+    const clientIp = this.getClientIp(request);
+    if (!ipRangeCheck(clientIp, this.allowedSubnet)) {
+      console.log('Requests from this IP are not allowed');
       throw new ForbiddenException('Requests from this IP are not allowed');
     }
 
     return true;
+  }
+
+  private getClientIp(request: Request): string {
+    const xForwardedFor = request.headers['x-forwarded-for'];
+    if (typeof xForwardedFor === 'string') {
+      return xForwardedFor.split(',')[0].trim();
+    }
+    return request.ip;
   }
 }
