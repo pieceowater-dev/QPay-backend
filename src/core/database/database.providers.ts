c78a9c967e16880c810f1c1db56f46c1444960f5
vitJR1 <kalugin.vitaliy23@gmail.com>
2024-09-22 14:52:41 +0500

    feat: log

--- src/core/database/database.providers.ts
+++ src/core/database/database.providers.ts
@@ -7,6 +7,7 @@ export const databaseProviders = [
     imports: [ConfigModule],
     provide: 'DATA_SOURCE',
     useFactory: async (configService: ConfigService) => {
+      console.log('Connecting to:', configService.get('typeorm'));
       return new DataSource(configService.get('typeorm'))
         .initialize()
         .then(async (source) => await databaseInit(source, configService));
